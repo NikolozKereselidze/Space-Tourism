9a89a3a193acc955b18e18f59272c78f77325aba
Nika <Nika@users-MacBook-Pro.local>
2024-12-30 15:29:12 +0400

    style: adjust Navbar padding structure for cleaner layout

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -6,14 +6,16 @@ const navigations: string[] = ["Home", "Destinations", "Crew", "Technology"];
 
 const Navbar = () => {
   return (
-    <div className={styles.navWrapper}>
-      <img src={logo} alt="Website logo" className={styles.logo} />
-      <div className={styles.navigation}>
-        {navigations.map((navigation: string) => (
-          <NavLink key={navigation} to={`/${navigation.toLowerCase()}`}>
-            {navigation}
-          </NavLink>
-        ))}
+    <div className={styles.navContainer}>
+      <div className={styles.navWrapper}>
+        <img src={logo} alt="Website logo" className={styles.logo} />
+        <div className={styles.navigation}>
+          {navigations.map((navigation: string) => (
+            <NavLink key={navigation} to={`/${navigation.toLowerCase()}`}>
+              {navigation}
+            </NavLink>
+          ))}
+        </div>
       </div>
     </div>
   );
